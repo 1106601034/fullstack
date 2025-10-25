47c40aa9aa74c5715bd471467178df9264302668
1106601034 <christopherchan3003@gmail.com>
2025-10-25 19:40:21 +1000

    update

--- image-uploader/Frontend/src/components/Message.tsx
+++ image-uploader/Frontend/src/components/Message.tsx
@@ -1,18 +1,19 @@
 import React from "react";
 
 const Message = ({ message, setMessage }) => {
-{
-  return (
-    <div className="alert alert-warning alert-dissmissable fade show">
-      {message}
-      <button
-        type="button"
-        className="btn-close"
-        aria-label="Close"
-        onClick={() => setMessage("")}
-      ></button>
-    </div>
-  );
+  {
+    return (
+      <div className="alert alert-warning alert-dissmissable fade show">
+        {message}
+        <button
+          type="button"
+          className="btn-close"
+          aria-label="Close"
+          onClick={() => setMessage("")}
+        ></button>
+      </div>
+    );
+  }
 };
 
 export default Message;
